6df89cd6767741a2e11dc1cd1fc1395c0a6d8880
showszs <pasativan152@gmail.com>
2024-11-16 18:37:43 +0300

    lesson30

---
+++ src/main.js
@@ -0,0 +1,38 @@
+console.log('#13. JavaScript homework example file')
+
+/*
+ * #1
+ *
+ * Написати функцію, яка приймає рядок як вхідний параметр і перевіряє, чи є цей рядок валідною електронною адресою за допомогою регулярного виразу.
+ * Функція повертає true, якщо електронна адреса валідна, і false в іншому випадку.
+ *
+ */
+
+function isValidEmail(email) {
+ return (/^[a-z0-9-.]+@[a-z]+\.[a-z]{2,3}$/).test(email)
+}
+
+console.log(isValidEmail('example@example.com')) // Повинно вивести: true
+console.log(isValidEmail('invalid-email'))       // Повинно вивести: false
+
+/*
+ * #2
+ *
+ * Написати функцію, яка приймає рядок як вхідний параметр і перевіряє, чи є цей рядок валідним URL веб-сайту за допомогою регулярного виразу.
+ * Функція повертає true, якщо URL валідний, і false в іншому випадку.
+ *
+ */
+
+/*
+
+*/
+
+function isValidUrl(url) {
+  return (/^[a-z]+\:\/\/[a-z.]*[a-z]+\.[a-z]{2,3}$/).test(url)
+}
+
+console.log(isValidUrl('https://www.example.com')) // Повинно вивести: true
+console.log(isValidUrl('invalid-url'))             // Повинно вивести: false
+
+
+export { isValidEmail, isValidUrl }
